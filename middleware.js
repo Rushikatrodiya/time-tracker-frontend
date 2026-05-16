617e0c6515cb0b0a9e3a152efae587c93af8d12c
Rushikatrodiya <your-github-email@example.com>
2026-05-16 15:32:55 +0530

    change middleware config

--- middleware.js
+++ middleware.js
@@ -49,5 +49,5 @@ export function middleware(request) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|backend|api).*)"],
 };
